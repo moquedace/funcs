de2d3aae2cd018d9adba5efeeca24c61eec4ca1a
Cássio Moquedace <67038094+moquedace@users.noreply.github.com>
2024-07-16 18:54:03 -0300

    Update bivariate_map.R

--- bivariate_map.R
+++ bivariate_map.R
@@ -198,6 +198,7 @@ bivariate_map <- function(rasterx, rastery, colourmatrix = col.matrix,
   cat("Todos os argumentos foram verificados e são válidos.\n")
   
   # Inicializa a paralelização
+  options(future.globals.maxSize = 30 * 1024^3)
   future::plan(future::multicore, workers = ncores)
   cat("Paralelização inicializada com", ncores, "núcleos.\n")
   
