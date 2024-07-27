91aff1f363e96ab46e8760ac3fbb507958b1d03e
Cássio Moquedace <67038094+moquedace@users.noreply.github.com>
2024-07-27 12:46:42 -0300

    Update pred_writer_qrf_raster.R

--- pred_writer_qrf_raster.R
+++ pred_writer_qrf_raster.R
@@ -19,7 +19,7 @@ pred_writer_qrf_raster <- function(model = NULL,
     if (length(to_install) > 0) {
       install.packages(to_install)
     }
-    invisible(lapply(packages, library, character.only = TRUE))
+    suppressPackageStartupMessages(lapply(packages, library, character.only = TRUE))
   }
   
   required_packages <- c("randomForest", "data.table", "microbenchmark",
